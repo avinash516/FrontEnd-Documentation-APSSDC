475b71fca7284305c79e83a76f85ed9921dbf6ee
Ch.Avinash <37656188+avinash516@users.noreply.github.com>
2020-08-10 11:49:45 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -127,7 +127,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'SCMFDP.tex', u'SCMFDP Documentation',
+    (master_doc, 'SCMFDP.tex', u'FrontEnd Documentation',
      u'author here', 'manual'),
 ]
 
@@ -137,7 +137,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'scmfdp', u'SCMFDP Documentation',
+    (master_doc, 'scmfdp', u'FrontEnd Documentation',
      [author], 1)
 ]
 
@@ -148,7 +148,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'SCMFDP', u'SCMFDP Documentation',
+    (master_doc, 'SCMFDP', u'FrontEnd Documentation',
      author, 'SCMFDP', 'One line description of project.',
      'Miscellaneous'),
 ]
